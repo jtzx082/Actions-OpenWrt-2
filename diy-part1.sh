3e357bd1ce38c988f91dfd14084b969c70ace37b
jtzx082 <yujq804@gmail.com>
2022-04-20 15:28:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,7 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+# Add package
+git clone https://github.com/kenzok8/small-package.git package/small8
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
